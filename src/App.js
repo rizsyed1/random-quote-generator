b6fabb2b26674eee0afe10d1c6b417e09772292c
RizwanSyed357 <rizwansyed357@protonmail.com>
2019-03-24 22:45:56 +0000

    Added new stateless components for the QuoteDisplay and AuthorName. Converted event handler functions to arrow functions and removed binding assignments in constructor function of parent component.

--- src/App.js
+++ src/App.js
@@ -16,6 +16,18 @@ function QuoteButton ({handleClick}) {
 	)
 }
 
+function QuoteDisplay({quote, className, quotationMark}) {
+	return (
+		<span className={className} ><FontAwesomeIcon quotationIcon={quotationMark} />{quote}</span>
+	)
+}
+
+function AuthorName ({className, name}) {
+	return (
+		<span className='Author'>{name}</span>
+	)
+}
+
 class App extends React.Component {
 	constructor(props) {
 		super(props);
@@ -24,33 +36,30 @@ class App extends React.Component {
 			currentAuthor: 'Author Loading',
 			color: 'red'
 		}
-		this.handleTwitterShare = this.handleTwitterShare.bind(this);
-		this.handleTumblrShare = this.handleTumblrShare.bind(this);
-		this.fetchData = this.fetchData.bind(this);
-		this.decodeQuote = this.decodeQuote.bind(this)
 	}
 
 
-	handleTwitterShare(){
+	handleTwitterShare = () =>
 		window.open('https://twitter.com/intent/tweet?hashtags=quotes&text=' + '"' + this.state.shareQuote + '"' + ' ' + this.state.currentAuthor)
-	}
 
-	handleTumblrShare(){
+
+	handleTumblrShare = () =>
 		window.open('https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes&caption=' + encodeURIComponent(this.state.currentAuthor) +'&content=' + encodeURIComponent(this.state.shareQuote)+'&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
-	}
 
-	componentDidMount() {
+
+	componentDidMount = () =>
 		this.fetchData();
-	}
 
 
-	decodeQuote (html) {
+
+	decodeQuote = (html) => {
 		let txt = document.createElement('textarea');
 		txt.innerHTML = html;
 		return txt.value;
 	}
 
-	fetchData () {
+
+	fetchData = () =>
 		fetch('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1', {cache: "no-store"})
 			.then(response => response.json())
 			.then(data => {
@@ -67,15 +76,17 @@ class App extends React.Component {
 					}
 			}
 			)
-	}
+
 
 	render() {
 		return (
       <div className='Container'>
   			<div className='QuoteBox'>
-						<span className='Quote'><FontAwesomeIcon icon={faQuoteLeft} />{this.state.shareQuote}</span>
+					<div className='QuoteElement'>
+						<QuoteDisplay className='quote' quotationMark={faQuoteLeft} quote={this.state.shareQuote} />
+					</div>
 					<div className='AuthorContainer'>
-						<span className='Author'>{this.state.currentAuthor}</span>
+						<AuthorName className='Author' name={this.state.currentAuthor} />
 					</div>
 					<div className='Buttons'>
 						<Icon className="fab fa-twitter-square" handleClick={this.handleTwitterShare} />
