32466447b107c660f38f6a64274ccc72b76b8678
RizwanSyed357 <rizwansyed357@protonmail.com>
2019-05-16 15:02:43 +0100

    removed test written as experiment

--- src/QuoteButton.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Hello from './Hello'
-import { shallow } from 'enzyme';
-
-it('renders an <input> element', () => {
-    const typeAttrib = shallow(<input/>);
-    expect(typeAttrib.find.attr("type='button'"))
-}
