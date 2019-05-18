4c6a1a05ec3797b4e2f2e942a07d552c78be173b
RizwanSyed357 <rizwansyed357@protonmail.com>
2019-05-18 15:01:57 +0100

    Updates

---
+++ precache-manifest.e47dcbfb54ad0edafb43bf73e3acb42a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e053ee27e90940af785398ce9c69cf1d",
+    "url": "/RizwanSyed357/random-quote-generator/index.html"
+  },
+  {
+    "revision": "fca95e95844c37e33094",
+    "url": "/RizwanSyed357/random-quote-generator/static/css/main.b0a4fd7c.chunk.css"
+  },
+  {
+    "revision": "90806a2cb24edfe981dc",
+    "url": "/RizwanSyed357/random-quote-generator/static/js/2.52a6816e.chunk.js"
+  },
+  {
+    "revision": "fca95e95844c37e33094",
+    "url": "/RizwanSyed357/random-quote-generator/static/js/main.7178e5eb.chunk.js"
+  },
+  {
+    "revision": "e73c9d5b57bb0d5782f0",
+    "url": "/RizwanSyed357/random-quote-generator/static/js/runtime~main.d71a4492.js"
+  }
+]);
